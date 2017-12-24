aba88ca3e87aff20dcbee718ed60642ce3cc960e
Karl J. Overå <karl@karl.run>
2017-12-24 15:24:09 +0100

    Fix structure and typings

--- api/src/static/static.js
+++ api/src/static/static.js
@@ -3,6 +3,7 @@ const express = require('express');
 const init = (app) => {
     const path = __dirname + '/../../../web/build';
 
+    console.log('env is ' + process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'production') {
         console.log("Serving static files from " + path)
         app.use(express.static(path));
---
+++ web/ts/squelch.d.ts
@@ -0,0 +1,2 @@
+// Temporary untill apollo-react fixes this: https://github.com/apollographql/react-apollo/issues/1286
+declare module 'lodash.flowright';
